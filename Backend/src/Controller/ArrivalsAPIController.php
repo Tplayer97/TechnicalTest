eb9eee7e6564872a11bb5ae7aef05ab407405ce3
Jose <jm@green4net.com>
2025-02-08 13:52:38 +0100

    Docker containers configured and ready to be built and run from the docker-compose in the root

--- Backend/src/Controller/ArrivalsAPIController.php
+++ Backend/src/Controller/ArrivalsAPIController.php
@@ -11,7 +11,7 @@ final class ArrivalsAPIController extends AbstractController
     #[Route('/arrivals', name: 'app_arrivals_a_p_i')]
     public function index(): Response
     {
-        dd('I am here');
+     
         return $this->render('arrivals_api/index.html.twig', [
             'controller_name' => 'ArrivalsAPIController',
         ]);
