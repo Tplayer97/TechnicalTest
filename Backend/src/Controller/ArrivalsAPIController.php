de3633c90d00077c350ecf34b05a4e28a1aed032
Jose <jm@green4net.com>
2025-02-08 11:27:48 +0100

    Docker configured and working for the Backend

---
+++ Backend/src/Controller/ArrivalsAPIController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ArrivalsAPIController extends AbstractController
+{
+    #[Route('/arrivals', name: 'app_arrivals_a_p_i')]
+    public function index(): Response
+    {
+        dd('I am here');
+        return $this->render('arrivals_api/index.html.twig', [
+            'controller_name' => 'ArrivalsAPIController',
+        ]);
+    }
+}
